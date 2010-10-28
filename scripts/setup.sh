340b1f0432bc5a499dae61cdff630dfcbb464152
Nilesh Kapadia <code@nileshk.com>
2010-10-27 21:35:59 -0400

    Moved styles out of index.html into screen.css

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,7 +23,7 @@ cp -R blueprint $ROOT_DIR/static/css
 cd ..
 rm blueprint.tar.gz
 rm -rf joshuaclayton-blueprint-css*
-curl -L -o $ROOT_DIR/static/js/jquery.ui.menu.js http://github.com/jquery/jquery-ui/raw/0746f991155df8a833abbe6ac1fbdcf6c73217ec/ui/jquery.ui.menu.js
-curl -L -o $ROOT_DIR/static/css/jquery.ui.menu.css http://github.com/jquery/jquery-ui/raw/menu/themes/base/jquery.ui.menu.css
-curl -L -o $ROOT_DIR/static/css/fg.menu.css http://jquery-ui.googlecode.com/svn/branches/labs/fg-menu/fg.menu.css
-curl -L -o $ROOT_DIR/static/js/fg.menu.js http://jquery-ui.googlecode.com/svn/branches/labs/fg-menu/fg.menu.js
+#curl -L -o $ROOT_DIR/static/js/jquery.ui.menu.js http://github.com/jquery/jquery-ui/raw/0746f991155df8a833abbe6ac1fbdcf6c73217ec/ui/jquery.ui.menu.js
+#curl -L -o $ROOT_DIR/static/css/jquery.ui.menu.css http://github.com/jquery/jquery-ui/raw/menu/themes/base/jquery.ui.menu.css
+#curl -L -o $ROOT_DIR/static/css/fg.menu.css http://jquery-ui.googlecode.com/svn/branches/labs/fg-menu/fg.menu.css
+#curl -L -o $ROOT_DIR/static/js/fg.menu.js http://jquery-ui.googlecode.com/svn/branches/labs/fg-menu/fg.menu.js
